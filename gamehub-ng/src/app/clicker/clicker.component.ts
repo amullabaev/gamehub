58a3427ac269a6a732b3e8539117e2ac66ac884f
Amirkhan Mullabaev <a.mullabaev@gmail.com>
2025-06-15 13:30:46 +0200

    add save button

--- gamehub-ng/src/app/clicker/clicker.component.ts
+++ gamehub-ng/src/app/clicker/clicker.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
 
 @Component({
   selector: 'app-clicker',
@@ -7,10 +8,21 @@ import { Component, signal } from '@angular/core';
   styleUrl: './clicker.component.scss',
 })
 export class ClickerComponent {
+
+  private http = inject(HttpClient);
   score = signal(0);
 
-  increment() {
+  protected increment() {
     console.log('Button clicked!');
     this.score.update((v) => v + 1);
+  }
+  
+  protected save() {
+    console.log('Score saved:', this.score());
+    const resultBody = {
+      score: this.score(),
+      timestamp: new Date().toISOString(),
+    }
+    this.http.post('/api/save/clicker', resultBody).subscribe();
   }
 }
